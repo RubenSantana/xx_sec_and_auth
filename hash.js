f108b4c8320cdfd244c29dfa6ea2b92670e67e58
RubenSantana <ruben_santana1990@hotmail.com>
2019-07-12 17:02:38 +0100

    first commit

---
+++ hash.js
@@ -0,0 +1,23 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   if (err) return next(err);
+//
+//   bcrypt.hash("password123", salt, (err, hash) => {
+//     if (err) return next(err);
+//
+//     console.log(hash);
+//   });
+// });
+
+const id = "1000";
+const secret = "supersecret";
+
+const receivedToken =
+  "eyJhbGciOiJIUzI1NiJ9.MTAwMA.L9PmEqLlZjettygguzj25agunJu6NkvVtG9RFRBnK2Y";
+
+const token = jwt.sign(id, secret);
+const decodeToken = jwt.verify(receivedToken, secret);
+
+console.log(decodeToken);
